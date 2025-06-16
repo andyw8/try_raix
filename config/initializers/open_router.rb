1635c8f3a67411d548b85563ed40bef494654942
Andy Waite <13400+andyw8@users.noreply.github.com>
2025-06-16 15:02:44 -0400

    Config

--- config/initializers/open_router.rb
+++
@@ -1,5 +0,0 @@
-OpenRouter.configure do |config|
-  config.access_token = ENV.fetch("OR_ACCESS_TOKEN")
-  config.site_name = "Olympia"
-  config.site_url = "https://olympia.chat"
-end
--- config/initializers/raix.rb
+++ config/initializers/raix.rb
@@ -1,3 +1,3 @@
 Raix.configure do |config|
-  config.openrouter_client = OpenRouter::Client.new
+  config.openrouter_client = OpenRouter::Client.new(access_token: ENV.fetch("OR_ACCESS_TOKEN"))
 end
