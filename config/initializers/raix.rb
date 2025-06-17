d22a1103be98beb032d0060ff32041bbfbb0476d
Andy Waite <13400+andyw8@users.noreply.github.com>
2025-06-16 21:17:09 -0400

    Enable logging, set max_tool_calls

--- config/initializers/raix.rb
+++ config/initializers/raix.rb
@@ -1,3 +1,10 @@
 Raix.configure do |config|
-  config.openrouter_client = OpenRouter::Client.new(access_token: ENV.fetch("OR_ACCESS_TOKEN"))
+  config.openrouter_client = OpenRouter::Client.new(access_token: ENV.fetch("OR_ACCESS_TOKEN")) do |config|
+    config.faraday do |f|
+      f.response :logger, ::Logger.new($stdout), { headers: true, bodies: true, errors: true } do |logger|
+        logger.filter(/(Bearer) (\S+)/, '\1[REDACTED]')
+      end
+    end
+  end
+  config.max_tool_calls = 10
 end
